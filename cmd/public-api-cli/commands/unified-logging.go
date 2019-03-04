adcf0e88c64656cc7dc0cc187299295753782fa5
Giel de Nijs <giel@daisho.group>
2019-03-04 13:10:30 +0100

    [NP-875] Add support for --asc and --desc

--- cmd/public-api-cli/commands/unified-logging.go
+++ cmd/public-api-cli/commands/unified-logging.go
@@ -6,6 +6,7 @@ package commands
 
 import (
 	"github.com/nalej/public-api/internal/app/cli"
+        "github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
@@ -30,6 +31,8 @@ func init() {
 	searchCmd.Flags().StringVar(&sgInstanceID, "sgInstanceID", "", "Service group instance identifier")
 	searchCmd.Flags().StringVar(&from, "from", "", "Start time of logs")
 	searchCmd.Flags().StringVar(&to, "to", "", "End time of logs")
+	searchCmd.Flags().BoolVar(&asc, "asc", false, "Sort results in ascending time order")
+	searchCmd.Flags().BoolVar(&desc, "desc", false, "Sort results in descending time order")
 	searchCmd.Flags().BoolVar(&redirectLog, "redirectResultAsLog", false, "Redirect the result to the CLI log")
 }
 
@@ -45,11 +48,19 @@ var searchCmd = &cobra.Command{
 			message = args[0]
 		}
 
+		// Verify sort order
+		// Default is ascending, so "--asc" is only used to make the command line
+		// very explicit. If "--desc" is set, we know "--asc" is not because of
+		// this check.
+		if asc && desc {
+			log.Fatal().Msg("Specify sort order as ascending OR descending - not both")
+		}
+
 		l := cli.NewUnifiedLogging(
 			options.Resolve("nalejAddress", nalejAddress),
 			options.ResolveAsInt("port", nalejPort),
 			insecure,
 			options.Resolve("cacert", caCertPath))
-		l.Search(options.Resolve("organizationID", organizationID), instanceID, sgInstanceID, message, from, to, redirectLog)
+		l.Search(options.Resolve("organizationID", organizationID), instanceID, sgInstanceID, message, from, to, desc, redirectLog)
 	},
 }
