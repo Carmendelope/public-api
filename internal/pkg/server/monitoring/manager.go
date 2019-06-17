431128b9592ae34a195a41e615223903adc273b7
Giel de Nijs <giel@daisho.group>
2019-06-17 09:13:47 -0700

    [NP-1362] Manager for metrics forwarding requests to inventory manager.

--- internal/pkg/server/monitoring/manager.go
+++ internal/pkg/server/monitoring/manager.go
@@ -5,26 +5,32 @@
 package monitoring
 
 import (
-	"github.com/nalej/derrors"
-
 	"github.com/nalej/grpc-inventory-manager-go"
+
+	"github.com/nalej/public-api/internal/pkg/server/common"
 )
 
 // Manager structure with the required clients for monitoring operations.
 type Manager struct {
-	client *grpc_inventory_manager_go.InventoryMonitoringClient
+	client grpc_inventory_manager_go.InventoryMonitoringClient
 }
 
 func NewManager(client grpc_inventory_manager_go.InventoryMonitoringClient) Manager {
 	return Manager{
-		client: &client,
+		client: client,
 	}
 }
 
 func (m *Manager) ListMetrics(selector *grpc_inventory_manager_go.AssetSelector) (*grpc_inventory_manager_go.MetricsList, error) {
-	return nil, derrors.NewUnimplementedError("ListMetrics not implemented")
+	ctx, cancel := common.GetContext()
+	defer cancel()
+
+	return m.client.ListMetrics(ctx, selector)
 }
 
 func (m *Manager) QueryMetrics(request *grpc_inventory_manager_go.QueryMetricsRequest) (*grpc_inventory_manager_go.QueryMetricsResult, error) {
-	return nil, derrors.NewUnimplementedError("QueryMetrics not implemented")
+	ctx, cancel := common.GetContext()
+	defer cancel()
+
+	return m.client.QueryMetrics(ctx, request)
 }
