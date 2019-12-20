228137a13969af6d0d17a1e945b5c92d08963dfa
Carmen de Lope Contreras <carmendelope@telefonica.net>
2019-12-20 15:56:51 +0100

    NP-XX include userID in the context passed to call unified-logging

--- internal/pkg/server/common/grpc.go
+++ internal/pkg/server/common/grpc.go
@@ -23,8 +23,10 @@ import (
 	"time"
 )
 
-const DefaultTimeout = time.Minute
-
+const (
+	DefaultTimeout = time.Minute
+	UserID = "userid"
+)
 // GetContext returns a context with a default timeout for internal communications. Notice that the context does not
 // have any security related information attached to it.
 func GetContext() (context.Context, context.CancelFunc) {
@@ -32,7 +34,7 @@ func GetContext() (context.Context, context.CancelFunc) {
 }
 
 func GetContextWithUser(userId string)(context.Context, context.CancelFunc){
-	md := metadata.New(map[string]string{"user_id": userId})
+	md := metadata.New(map[string]string{UserID: userId})
 	log.Debug().Interface("md", md).Msg("metadata has been created")
 	baseContext, cancel := context.WithTimeout(context.Background(), DefaultTimeout)
 	return metadata.NewOutgoingContext(baseContext, md), cancel
