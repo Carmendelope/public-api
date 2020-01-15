ad2d56e18e2231b217cef171c70273fa45183c06
Carmen de Lope Contreras <carmendelope@telefonica.net>
2020-01-15 11:14:46 +0100

    NP-2527 organization-settings handler and manager

---
+++ internal/pkg/server/organization-settings/handler.go
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 Nalej
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package organization_settings
+
+import (
+	"context"
+	"github.com/nalej/derrors"
+	"github.com/nalej/grpc-common-go"
+	"github.com/nalej/grpc-organization-go"
+	"github.com/nalej/grpc-organization-manager-go"
+	"github.com/nalej/grpc-public-api-go"
+	"github.com/nalej/grpc-utils/pkg/conversions"
+	"github.com/nalej/public-api/internal/pkg/authhelper"
+	"github.com/nalej/public-api/internal/pkg/entities"
+)
+
+// Handler structure for the organizations requests.
+type Handler struct {
+	Manager Manager
+}
+
+// NewHandler creates a new Handler with a linked manager.
+func NewHandler(manager Manager) *Handler {
+	return &Handler{manager}
+}
+
+
+func (h *Handler) Update(ctx context.Context, updateRequest *grpc_public_api_go.UpdateSettingRequest) (*grpc_common_go.Success, error) {
+	rm, err := authhelper.GetRequestMetadata(ctx)
+	if err != nil {
+		return nil, conversions.ToGRPCError(err)
+	}
+	if updateRequest.OrganizationId != rm.OrganizationID {
+		return nil, derrors.NewPermissionDeniedError("cannot access requested OrganizationID")
+	}
+
+	vErr := entities.ValidUpdateSettingRequest(updateRequest)
+	if vErr != nil {
+		return nil, conversions.ToGRPCError(vErr)
+	}
+
+	return h.Manager.Update(updateRequest)
+}
+func (h *Handler) List(ctx context.Context, orgID *grpc_organization_go.OrganizationId) (*grpc_organization_manager_go.SettingList, error){
+	rm, err := authhelper.GetRequestMetadata(ctx)
+	if err != nil {
+		return nil, conversions.ToGRPCError(err)
+	}
+	if orgID.OrganizationId != rm.OrganizationID {
+		return nil, derrors.NewPermissionDeniedError("cannot access requested OrganizationID")
+	}
+
+	vErr := entities.ValidOrganizationId(orgID)
+	if vErr != nil {
+		return nil, conversions.ToGRPCError(vErr)
+	}
+
+	return h.Manager.List(orgID)
+}
